5494ff0f18b7e369d4e8134afe958e1a565c0ade
voklorok <korolkovac@gmail.com>
2013-10-01 22:51:23 +0400

    test_resource

--- src/main/java/resource/FightResource.java
+++ src/main/java/resource/FightResource.java
@@ -6,7 +6,6 @@ public class FightResource implements Resource {
 
     public int healthMax;
     public int fireballDamage;
-
     public int playerWidth;
     public int playerHeight;
 
--- src/main/java/resource/MapResource.java
+++ src/main/java/resource/MapResource.java
@@ -4,8 +4,7 @@ import base.Resource;
 
 public class MapResource implements Resource {
 
-    public int width,
-               height;
+    public int    width, height;
     public String name;
 
 }
