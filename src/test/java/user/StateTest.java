cf2f4215ff8e699d8efe93551831824a8238763a
ShevtsovAleksey <shevtsov.aleksey.sergeevich@yandex.ru>
2013-10-20 00:35:50 +0400

    user tests

--- src/test/java/user/StateTest.java
+++ src/test/java/user/StateTest.java
@@ -24,77 +24,31 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
-/**
- * Created with IntelliJ IDEA.
- * User: korolkov
- * Date: 10/15/13
- * Time: 12:06 PM
- * To change this template use File | Settings | File Templates.
- */
 @RunWith(MockitoJUnitRunner.class)
 public class StateTest {
-
-
-    FrontendImpl               frontend;
-    @Mock HttpServletRequest   request;
-    @Mock HttpServletResponse  response;
+    @Mock HttpServletRequest   servletRequest;
+    @Mock HttpServletResponse  servletResponse;
     @Mock Request              baseRequest;
+    @Mock UserSession          userSession;
     @Mock PrintWriter          printWriter;
-    @Mock MessageSystem        msgSystem;
-	@Mock UserSession           session;
+    @Mock MessageSystem        messageSystem;
+    FrontendImpl               frontend;
 
-    LongId<User>         userGameNumber    =  new LongId<User>(1);
-    LongId<UserSession>  userSessionNumber =  new LongId<UserSession>(1);
+    LongId<User>         gameNumber =  new LongId<User>(1);
+    LongId<UserSession>  sessionNumber =  new LongId<UserSession>(1);
     String               userSessionName   =  "user_name";
     Integer              userHealth        =  100;
-
-
-    @Before
-    public void setUp() throws Exception {
-        //request
-        when(request.getRequestURI()).thenReturn("request");
-        when(request.getCookies()).thenReturn(new Cookie[0]);
-        when(request.getParameter((anyString()))).thenReturn("0");
-        when(request.getParameter("showChats")).thenReturn("yes");
-        when(request.getParameter("Exit")).thenReturn("Exit");
-
-        when(request.getMethod()).thenReturn("POST");
-
-        when(request.getMethod()).thenReturn("POST");//.thenReturn("POST");
-
-
-        //response
-        when(response.getWriter()).thenReturn(printWriter);
-        //msgSystem
-        when(msgSystem.execForAbonent(frontend)).thenReturn(true);
-        when(msgSystem.getAddressService()).thenReturn(null);
-        when(msgSystem.getAddress((Class<?>) any())).thenReturn(null);
-        when(msgSystem.execForAbonent((Abonent)any())).thenReturn(true);
-        doNothing().when(msgSystem).sendMessage((Message)any());
-        doNothing().when(msgSystem).addService((Class<?>)any(),(Abonent)any());
-
-        frontend = new FrontendImpl(msgSystem);
-        frontend.addUserSession(userSessionNumber, userSessionName);
-        frontend.addUserIdToSession(userGameNumber, userSessionNumber);
-        frontend.getUserSession(userGameNumber).setPlayer(new Player());
-
-	    Set<LongId<GameSession>> gamIdSet = new HashSet<LongId<GameSession>>();
-	    gamIdSet.add(new LongId<GameSession>(1));
-	    frontend.getUserSession(userGameNumber).setAvailableGameSessions(gamIdSet);
-	    frontend.getUserSessionBySessionId(userSessionNumber).setUserId(userGameNumber);
-
-    }
 
 
     @Test
     public void authorized() throws IOException {
 	    StateAuthorized stateAuthorized = new StateAuthorized();
-	    stateAuthorized.processUserState(frontend,userSessionNumber,request,response);
+	    stateAuthorized.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
-	    Assert.assertEquals("[1]", stateAuthorized.getLinkForAvailableGames(frontend, userSessionNumber));
+	    Assert.assertEquals("[1]", stateAuthorized.getLinkForAvailableGames(frontend, sessionNumber));
 
-	    frontend.getUserSession(userGameNumber).setAvailableGameSessions(new HashSet<LongId<GameSession>>());
-	    Assert.assertEquals("[]", stateAuthorized.getLinkForAvailableGames(frontend, userSessionNumber));
+	    frontend.getUserSession(gameNumber).setAvailableGameSessions(new HashSet<LongId<GameSession>>());
+	    Assert.assertEquals("[]", stateAuthorized.getLinkForAvailableGames(frontend, sessionNumber));
 
 	    Frontend frontend1 = mock(Frontend.class);
 	    UserSession session1 = mock(UserSession.class);
@@ -103,37 +57,77 @@ public class StateTest {
 	    when(session1.getUserId()).thenReturn(new LongId<User>(1));
 
 
-//	    when(frontend1.getUserSessionBySessionId(userSessionNumber)).thenReturn(session1);
-//	    stateAuthorized.getLinkForAvailableGames(frontend1, userSessionNumber);
+//	    when(frontend1.getUserSessionBySessionId(sessionNumber)).thenReturn(session1);
+//	    stateAuthorized.getLinkForAvailableGames(frontend1, sessionNumber);
 
 //	    when(frontend.chatSelected((HttpServletRequest) any(), (LongId<UserSession>) any())).thenReturn(true);
-//	    stateAuthorized.getLinkForAvailableGames(frontend1, userSessionNumber);
+//	    stateAuthorized.getLinkForAvailableGames(frontend1, sessionNumber);
 
     }
     @Test
     public void finishLose() throws IOException {
 	    StateFinishLose stateFinishLose = new StateFinishLose();
-	    stateFinishLose.processUserState(frontend, userSessionNumber, request, response);
+	    stateFinishLose.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
-	    when(request.getParameter("game")).thenReturn(null);
-	    stateFinishLose.processUserState(frontend, userSessionNumber, request, response);
+	    when(servletRequest.getParameter("game")).thenReturn(null);
+	    stateFinishLose.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
-	    when(request.getParameter("refresh")).thenReturn("fail");
-	    stateFinishLose.processUserState(frontend, userSessionNumber, request, response);
+	    when(servletRequest.getParameter("refresh")).thenReturn("fail");
+	    stateFinishLose.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
-	    when(request.getParameter("refresh")).thenReturn("ok");
-	    stateFinishLose.processUserState(frontend, userSessionNumber, request, response);
+	    when(servletRequest.getParameter("refresh")).thenReturn("ok");
+	    stateFinishLose.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
     }
+
+
+    @Before
+    public void setUp() throws Exception {
+        //servletRequest
+        when(servletRequest.getRequestURI()).thenReturn("servletRequest");
+        when(servletRequest.getCookies()).thenReturn(new Cookie[0]);
+        when(servletRequest.getParameter((anyString()))).thenReturn("0");
+        when(servletRequest.getParameter("showChats")).thenReturn("yes");
+        when(servletRequest.getParameter("Exit")).thenReturn("Exit");
+
+        when(servletRequest.getMethod()).thenReturn("POST");
+
+        when(servletRequest.getMethod()).thenReturn("POST");//.thenReturn("POST");
+
+
+        //servletResponse
+        when(servletResponse.getWriter()).thenReturn(printWriter);
+
+        //messageSystem
+        when(messageSystem.execForAbonent(frontend)).thenReturn(true);
+        when(messageSystem.getAddressService()).thenReturn(null);
+        when(messageSystem.getAddress((Class<?>) any())).thenReturn(null);
+        when(messageSystem.execForAbonent((Abonent)any())).thenReturn(true);
+        doNothing().when(messageSystem).sendMessage((Message)any());
+        doNothing().when(messageSystem).addService((Class<?>)any(),(Abonent)any());
+
+        frontend = new FrontendImpl(messageSystem);
+        frontend.addUserSession(sessionNumber, userSessionName);
+        frontend.addUserIdToSession(gameNumber, sessionNumber);
+        frontend.getUserSession(gameNumber).setPlayer(new Player());
+
+        Set<LongId<GameSession>> gamIdSet = new HashSet<LongId<GameSession>>();
+        gamIdSet.add(new LongId<GameSession>(1));
+        frontend.getUserSession(gameNumber).setAvailableGameSessions(gamIdSet);
+        frontend.getUserSessionBySessionId(sessionNumber).setUserId(gameNumber);
+
+    }
+
+
     @Test
     public void finishWin() throws IOException {
 	    StateFinishWin stateFinishWin = new StateFinishWin();
-	    stateFinishWin.processUserState(frontend, userSessionNumber, request, response);
+	    stateFinishWin.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
-	    when(request.getParameter("game")).thenReturn(null);
-	    stateFinishWin.processUserState(frontend, userSessionNumber, request, response);
+	    when(servletRequest.getParameter("game")).thenReturn(null);
+	    stateFinishWin.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
-	    when(request.getParameter("refresh")).thenReturn("fail");
-	    stateFinishWin.processUserState(frontend, userSessionNumber, request, response);
+	    when(servletRequest.getParameter("refresh")).thenReturn("fail");
+	    stateFinishWin.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
 	    FrontendImpl frontend1 = mock(FrontendImpl.class);
 	    UserSession session1 = mock(UserSession.class);
@@ -141,47 +135,47 @@ public class StateTest {
 	    when(session1.getUserId()).thenReturn(new LongId<User>(1));
 
 
-	    when(frontend1.getUserSessionBySessionId(userSessionNumber)).thenReturn(session1);
-	    when(request.getParameter("refresh")).thenReturn("ok");
-	    new StateFinishWin().processUserState(frontend,userSessionNumber,request,response);
+	    when(frontend1.getUserSessionBySessionId(sessionNumber)).thenReturn(session1);
+	    when(servletRequest.getParameter("refresh")).thenReturn("ok");
+	    new StateFinishWin().processUserState(frontend, sessionNumber, servletRequest, servletResponse);
     }
 
     @Test
     public void play() throws IOException {
 	    StatePlay statePlay =  new StatePlay();
-	    statePlay.processUserState(frontend, userSessionNumber, request, response);
+	    statePlay.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
-	    when(request.getParameter("action")).thenReturn(null);
-	    statePlay.processUserState(frontend, userSessionNumber, request, response);
+	    when(servletRequest.getParameter("action")).thenReturn(null);
+	    statePlay.processUserState(frontend, sessionNumber, servletRequest, servletResponse);
 
-	    when(request.getMethod()).thenReturn("GET");
-	    when(request.getParameter("Exit")).thenReturn(null);
+	    when(servletRequest.getMethod()).thenReturn("GET");
+	    when(servletRequest.getParameter("Exit")).thenReturn(null);
 	    FrontendImpl frontend1 = mock(FrontendImpl.class);
-	    doNothing().doThrow(new RuntimeException()).when(frontend1).updateUserState((LongId<UserSession>) any(), (UserState) any());
-	    when(frontend1.processGameStep((HttpServletRequest) any(), (LongId<UserSession>) any())).thenReturn(null);
+	    doNothing().doThrow(new RuntimeException()).when(frontend1).updateUserState(( LongId<UserSession> ) any(), ( UserState ) any());
+	    when(frontend1.processGameStep(( HttpServletRequest ) any(), ( LongId<UserSession> ) any())).thenReturn(null);
 	    UserSession session1 = new UserSession("name");
 	    session1.setUserId(new LongId<User>(1));
 
-	    when(frontend1.getUserSessionBySessionId(userSessionNumber)).thenReturn(new UserSession("user_name"));
-	    statePlay.processUserState(frontend1, userSessionNumber, request, response);
+	    when(frontend1.getUserSessionBySessionId(sessionNumber)).thenReturn(new UserSession("user_name"));
+	    statePlay.processUserState(frontend1, sessionNumber, servletRequest, servletResponse);
 
-	    when(frontend1.processGameStep((HttpServletRequest) any(),  (LongId<UserSession>) any())).thenReturn("ok");
-	    statePlay.processUserState(frontend1, userSessionNumber, request, response);
+	    when(frontend1.processGameStep(( HttpServletRequest ) any(), ( LongId<UserSession> ) any())).thenReturn("ok");
+	    statePlay.processUserState(frontend1, sessionNumber, servletRequest, servletResponse);
 
 
-	    when(request.getParameter("action")).thenReturn("f");
-	    statePlay.processUserState(frontend1, userSessionNumber, request, response);
+	    when(servletRequest.getParameter("action")).thenReturn("f");
+	    statePlay.processUserState(frontend1, sessionNumber, servletRequest, servletResponse);
 
-//	    when(request.getParameter("Exit")).thenReturn("Exit");
-//	    statePlay.processUserState(frontend1, userSessionNumber, request, response);
+//	    when(servletRequest.getParameter("Exit")).thenReturn("Exit");
+//	    statePlay.processUserState(frontend1, sessionNumber, servletRequest, servletResponse);
     }
     @Test
     public void notAuth() throws IOException {
-        new StateNotAuthorized().processUserState(frontend,userSessionNumber,request,response);
+        new StateNotAuthorized().processUserState(frontend, sessionNumber, servletRequest, servletResponse);
     }
     @Test
     public void statеWait() throws IOException {
-        new StateWaitForAuth().processUserState(frontend,userSessionNumber,request,response);
+        new StateWaitForAuth().processUserState(frontend, sessionNumber, servletRequest, servletResponse);
     }
 
 	@Test
@@ -189,15 +183,15 @@ public class StateTest {
 		boolean retVal;
 		StatePlay state = new StatePlay();
 
-		retVal = state.chatExit(frontend, request, userSessionNumber);
+		retVal = state.chatExit(frontend, servletRequest, sessionNumber);
 		Assert.assertTrue(retVal);
 
-		when(request.getParameter("Exit")).thenReturn(null);
-		retVal = state.chatExit(frontend, request, userSessionNumber);
+		when(servletRequest.getParameter("Exit")).thenReturn(null);
+		retVal = state.chatExit(frontend, servletRequest, sessionNumber);
 		Assert.assertFalse(retVal);
 
-		when(request.getParameter("Exit")).thenReturn("FAIL");
-		retVal = state.chatExit(frontend, request, userSessionNumber);
+		when(servletRequest.getParameter("Exit")).thenReturn("FAIL");
+		retVal = state.chatExit(frontend, servletRequest, sessionNumber);
 		Assert.assertFalse(retVal);
 	}
 }
