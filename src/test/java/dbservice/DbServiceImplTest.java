b40da3287bb402c96d4b2e69cf55fcaa591aed6f
ShevtsovAleksey <shevtsov.aleksey.sergeevich@yandex.ru>
2013-10-20 00:23:48 +0400

    base tests

--- src/test/java/dbservice/DbServiceImplTest.java
+++ src/test/java/dbservice/DbServiceImplTest.java
@@ -15,48 +15,43 @@ import user.UserSession;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
-/**
- * Created with IntelliJ IDEA.
- * User: korolkov
- * Date: 10/17/13
- * Time: 1:02 AM
- * To change this template use File | Settings | File Templates.
- */
 @RunWith(MockitoJUnitRunner.class)
 public class DbServiceImplTest extends Assert {
-    DbServiceImpl dbService;
     @Mock MessageSystem messageSystem;
+    DbServiceImpl dbService;
+
 
     @Before
     public void setUp() throws Exception {
         dbService = new DbServiceImpl(messageSystem);
     }
 
+    @Test
+    public void getMessageSystem(){
+        assertTrue(dbService.getMessageSystem() instanceof MessageSystem);
+    }
 
     @Test
     public void getAddress(){
         assertTrue(dbService.getAddress() instanceof Address);
     }
+
+
     @Test
-    public void getMsgSystem(){
-        assertTrue(dbService.getMessageSystem() instanceof MessageSystem);
-    }
+	public void answerTest() {
+		dbService.getUserIdAndAnswer(new LongId<UserSession>(1), "Test");
+	}
 
     @Test
     public void getUsrId(){
         assertTrue(dbService.getUserId("Rus").get() == (new Long(2).longValue()));
     }
 
-	@Test
-	public void aswerTest() {
-		dbService.getUserIdAndAnswer(new LongId<UserSession>(1), "Test");
-	}
-
-	@Test
+    @Test
 	public void runTest() {
-		Thread gm = new Thread(dbService);
-		gm.start();
-		gm.interrupt();
+		Thread thread = new Thread(dbService);
+		thread.start();
+		thread.interrupt();
 	}
 
 }
